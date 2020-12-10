c451d6ee3ec38d1b6cabc1e015c2f15447d53962
jhonatan edward <jhonatanedward@air-de-jhonatan.partner.bs.br.bsch>
2020-12-10 15:50:22 -0300

    Implementando ordenacao

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -44,9 +45,9 @@ public class TopicosController {
 	// Cada metodo roda dentro de uma transação
 	@GetMapping
 	public Page<TopicoDto> lista(@RequestParam(required=false) String nomeCurso,
-			@RequestParam int pagina, @RequestParam int quantidadePorPagina
+			@RequestParam int pagina, @RequestParam int quantidadePorPagina, String ordenacao
 			) {
-		Pageable paginacao = PageRequest.of(pagina, quantidadePorPagina);
+		Pageable paginacao = PageRequest.of(pagina, quantidadePorPagina, Direction.ASC, ordenacao);
 		
 		if (nomeCurso == null) {
 			Page<Topico> topicos = topicosRepository.findAll(paginacao);
