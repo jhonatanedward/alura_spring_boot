48e8b171bd0d04ad70e082e45ef23311e84f3e78
Jhonatan Edward <jhonatanedsp@gmail.com>
2021-01-22 21:21:33 -0300

    Implementando segurança na API com Spring Security

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,23 +8,30 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 	// Metodo que configura autenticação
+	@Autowired
+	private AutenticacaoService autenticacaoService;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
-	}
+		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
+	}	
 	
 	// Configurações de autorizacao, perfis de acesso URL ETC
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
-		.antMatchers(HttpMethod.GET, "/topico/*").permitAll();
-		
+		.antMatchers(HttpMethod.GET, "/topico/*").permitAll()
+		.anyRequest()
+		.authenticated()
+		.and()
+		.formLogin();
 	}
 	
 	// Configuração de recursos estaticos, requisicoes para CSS HTML Imagens ETC.
